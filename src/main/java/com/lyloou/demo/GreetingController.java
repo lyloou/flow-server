ef4c64941a8a9f6929a2b056c0232b920efb4cd8
lilou <lyloou@qq.com>
2019-07-29 17:12:44 +0800

    update GreetingController

--- src/main/java/com/lyloou/demo/GreetingController.java
+++ src/main/java/com/lyloou/demo/GreetingController.java
@@ -8,20 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+
+    private final LongAdder counter = new LongAdder();
 
     @Autowired
     private ResultHandler resultHandler;
 
     @RequestMapping("/greeting")
     public Result greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        Greeting greeting = new Greeting(counter.incrementAndGet(),
+        counter.increment();
+        Greeting greeting = new Greeting(counter.longValue(),
                 String.format(template, name));
         return resultHandler.dataResult(() -> Constants.STATUS_OK, greeting);
     }
