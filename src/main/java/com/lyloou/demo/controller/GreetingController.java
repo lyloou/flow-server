3060258359434ec127f7bbce293c28169532192b
lilou <lyloou@qq.com>
2019-07-30 13:50:16 +0800

    update: logger

--- src/main/java/com/lyloou/demo/controller/GreetingController.java
+++ src/main/java/com/lyloou/demo/controller/GreetingController.java
@@ -4,7 +4,9 @@ import com.lyloou.common.exception.CommonException;
 import com.lyloou.common.status.ResultHandler;
 import com.lyloou.common.status.Result;
 import com.lyloou.common.constant.StatusConstants;
-import com.lyloou.demo.Greeting;
+import com.lyloou.demo.model.greeting.GreetingVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,15 +21,20 @@ public class GreetingController {
 
     private final LongAdder counter = new LongAdder();
 
+    private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
     @Autowired
     private ResultHandler resultHandler;
 
     @RequestMapping("/greeting")
     public Result greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.info("logger-info", "asdfasdf");
+        logger.debug("logger-debug", "asdfasdf");
+        logger.warn("logger-warn", "asdfasdf");
+        logger.error("logger-error", "asdfasdf");
         counter.increment();
-        Greeting greeting = new Greeting(counter.longValue(),
+        GreetingVO greetingVO = new GreetingVO(counter.longValue(),
                 String.format(template, name));
-        return resultHandler.dataResult(() -> StatusConstants.STATUS_OK, greeting);
+        return resultHandler.dataResult(() -> StatusConstants.STATUS_OK, greetingVO);
     }
 
     @RequestMapping("/exception")
--- src/main/java/com/lyloou/demo/model/greeting/GreetingVO.java
+++ src/main/java/com/lyloou/demo/model/greeting/GreetingVO.java
@@ -1,11 +1,11 @@
-package com.lyloou.demo;
+package com.lyloou.demo.model.greeting;
 
-public class Greeting {
+public class GreetingVO {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public GreetingVO(long id, String content) {
         this.id = id;
         this.content = content;
     }
