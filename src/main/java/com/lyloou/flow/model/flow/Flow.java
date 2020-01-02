76d5368f2b8a5997230e15b22d578018bc843bdf
lilou <lyloou@qq.com>
2020-01-02 18:49:09 +0800

    update flow

--- src/main/java/com/lyloou/flow/model/flow/Flow.java
+++ src/main/java/com/lyloou/flow/model/flow/Flow.java
@@ -1,12 +1,20 @@
 package com.lyloou.flow.model.flow;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 
 /**
  * @author lyloou
  */
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Flow {
     private Long id;
 
@@ -22,59 +30,4 @@ public class Flow {
 
     private Boolean isDisabled;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Date getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public String getItem() {
-        return item;
-    }
-
-    public void setItem(String item) {
-        this.item = item;
-    }
-
-    public Boolean getArchived() {
-        return isArchived;
-    }
-
-    public void setArchived(Boolean archived) {
-        isArchived = archived;
-    }
-
-    public Boolean getDisabled() {
-        return isDisabled;
-    }
-
-    public void setDisabled(Boolean disabled) {
-        isDisabled = disabled;
-    }
 }
