099f69618620559e4e568da14f9ffebaf78853c0
lilou <lyloou@qq.com>
2020-03-20 07:47:17 +0800

    update: HttpTest.java

--- src/test/java/com/lyloou/http/HttpTest.java
+++ src/test/java/com/lyloou/http/HttpTest.java
@@ -1,10 +1,8 @@
 package com.lyloou.http;
 
 import com.lyloou.common.http.HttpClient;
-import org.junit.Test;
 
 public class HttpTest {
-    @Test
     public void test1() {
         HttpClient httpClient = new HttpClient();
         GitRepos gitRepos = httpClient.get("https://api.github.com/repos/lyloou/lou", GitRepos.class);
