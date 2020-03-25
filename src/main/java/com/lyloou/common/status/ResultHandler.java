46344ca48fc5ba9ad306af883f0bcdb348798f6d
lilou <lyloou@qq.com>
2020-03-26 07:14:49 +0800

    add: IdGenerator.java

--- src/main/java/com/lyloou/common/status/ResultHandler.java
+++ src/main/java/com/lyloou/common/status/ResultHandler.java
@@ -6,6 +6,14 @@ public interface ResultHandler {
 
     Result msgResult(StatusCode code);
 
+    /**
+     * 正常返回时，才使用这个；
+     * 错误的时候，不要用这个。填写data，有可能会导致解析不对；
+     *
+     * @param code 错误码
+     * @param data 数据
+     * @return 封闭的结果
+     */
     Result dataResult(StatusCode code, Object data);
 
 }
---
+++ src/main/java/com/lyloou/common/util/IdGenerator.java
@@ -0,0 +1,67 @@
+package com.lyloou.common.util;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+
+import java.util.Date;
+
+/**
+ * ID 生成器
+ */
+@Component
+public class IdGenerator {
+    enum Type {
+        NORMAL(""),
+        ORDER("OR"),
+        PAYMENT("PM"),
+        WALLET("WA"),
+        RETURN("RT"),
+        REFUND("RF");
+
+        private String prefix;
+
+        Type(String prefix) {
+            this.prefix = prefix;
+        }
+
+        public String getPrefix() {
+            return prefix;
+        }
+    }
+
+    private static String GENERATOR = "GENERATOR";
+    private static String FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static String FORMAT_1 = "yyyyMMddHHmmss";
+    private static String FORMAT_DATE = "yyyyMMdd";
+
+    public String generate(Type type) {
+        String now = DateFormatUtils.format(new Date(), FORMAT_1);
+        String key = GENERATOR + ":" + type.name();
+        int counter = getCounter(key);
+        return String.format("%s%s", now, counter);
+    }
+
+    @Autowired
+    Jedis jedis;
+
+    /**
+     * 通过 redis 来获取累加器
+     * 根据 key 类型来获取累加数字
+     *
+     * @param key 类型
+     * @return 累加的数字
+     */
+    private int getCounter(String key) {
+        int num;
+        String serial = jedis.get(key);
+        num = 1000;
+        if (serial != null && Integer.parseInt(serial) < 9999) {
+            num = jedis.incrBy(key, 1).intValue();
+        } else {
+            jedis.set(key, String.valueOf(num));
+        }
+        return num;
+    }
+}
